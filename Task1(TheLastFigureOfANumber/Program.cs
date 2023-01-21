f2e719c5c9040a55d67099f00c5b12d92026814a
Kate V <katsiarynait@gmail.com>
2023-01-21 22:56:07 +0300

    решено

--- Task1(TheLastFigureOfANumber/Program.cs
+++ Task1(TheLastFigureOfANumber/Program.cs
@@ -1,9 +1,19 @@
-﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+﻿/* Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
 456 -> 6
 782 -> 2
-918 -> 8
+918 -> 8 */
 
-Console.WriteLine("введите 3-значное число");
-string input = Console.ReadLine();
-int input = Convert.ToInt32(input);
-System.Console.WriteLine(input);
+int InputStringToInt(string message)
+{
+    Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+
+int number = InputStringToInt("введите 3-значное число");
+if (number < 99 || number > 999)
+{
+    System.Console.WriteLine("число не трёхзначное");
+}
+int theThirdFigure = number % 10;
+System.Console.WriteLine(theThirdFigure);
