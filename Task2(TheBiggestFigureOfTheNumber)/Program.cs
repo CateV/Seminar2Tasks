d2fc32d536fcc44ec0387ea808b0df7aaeb97e60
Kate V <katsiarynait@gmail.com>
2023-01-22 00:08:19 +0300

    решила сама

---
+++ Task2(TheBiggestFigureOfTheNumber)/Program.cs
@@ -0,0 +1,16 @@
+﻿/*2. Напишите программу, которая выводит случайное число из отрезка [10, 99] 
+и показывает наибольшую цифру числа.
+78 -> 8 
+12-> 2 
+85 -> 8
+*/
+int randomNumber = new Random().Next(10, 100);
+System.Console.WriteLine($"В случайном числе {randomNumber} ");
+int lastDigit = randomNumber % 10;
+int firstDigit = randomNumber / 10;
+int max = firstDigit;
+if (lastDigit > firstDigit)
+{
+    max = lastDigit;
+}
+System.Console.WriteLine($"наибольшая цифра - {max}");
